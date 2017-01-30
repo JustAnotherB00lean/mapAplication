5987c145382421c279a4d05275cdb477dd5857f5
administrador <administrador@DESKTOP-NG789FE>
2017-01-30 14:48:30 -0600

    Initialize of project

---
+++ BHI.MapApp/BHI.MapApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BHI.MapApp.Startup))]
+namespace BHI.MapApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
