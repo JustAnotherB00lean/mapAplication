6af6ee7d401918c1b9d8e41c3050fe7694b2dd57
administrador <administrador@VEGAB>
2017-01-31 10:02:11 -0600

    changed text

--- BHI.MapApp/BHI.MapApp/Controllers/HomeController.cs
+++ BHI.MapApp/BHI.MapApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BHI.MapApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Hola";
+            ViewBag.Message = "Hello";
 
             return View();
         }
